a03502e49aea7aa03e10e109fdd11838d30ccbc9
LM Bibera <>
2023-05-24 22:18:32 +0800

    Fix broken startup due to missing database

--- src/test/java/dev/codefactory/sandbox/SandboxApplicationTests.java
+++ src/test/java/dev/codefactory/sandbox/SandboxApplicationTests.java
@@ -2,9 +2,28 @@ package dev.codefactory.sandbox;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class SandboxApplicationTests {
+
+	@Container
+	public static final PostgreSQLContainer<?> db = new PostgreSQLContainer("postgres:15")
+			.withDatabaseName("sandbox")
+			.withUsername("sandbox")
+			.withPassword("sandbox");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", db::getJdbcUrl);
+		registry.add("spring.datasource.username", db::getUsername);
+		registry.add("spring.datasource.password", db::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
