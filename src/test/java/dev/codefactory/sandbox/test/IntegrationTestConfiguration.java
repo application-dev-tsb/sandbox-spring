2bf7389fb7d1bdaf09987eb6b5406532c55f2b8d
LM Bibera <>
2023-06-18 20:30:20 +0800

    Add TODO

--- src/test/java/dev/codefactory/sandbox/test/IntegrationTestConfiguration.java
+++ src/test/java/dev/codefactory/sandbox/test/IntegrationTestConfiguration.java
@@ -23,4 +23,16 @@ public class IntegrationTestConfiguration {
         return new KafkaContainer(parse("confluentinc/cp-kafka:7.4.0"));
     }
 
+    // TODO figure out how to add a custom container to the test context
+//    @Bean
+//    GenericContainer<?> kafkaSchemaRegistry() {
+//        Map<String, String> env = Map.of(
+//                "SCHEMA_REGISTRY_HOST_NAME", "schema_registry",
+//                "SCHEMA_REGISTRY_KAFKASTORE_CONNECTION_URL", "schema_registry",
+//                "SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", "schema_registry",
+//                "SCHEMA_REGISTRY_ACCESS_CONTROL_ALLOW_ORIGIN", "schema_registry"
+//        );
+//        return new GenericContainer<>(parse("confluentinc/cp-schema-registry:7.4.0")).withEnv(env);
+//    }
+
 }
